27b9135212c5828b4f43ec1077189c87df84608c
Brody Berg <brodyberg@gmail.com>
2018-04-13 14:53:47 -0700

    Use font-awesome for stars

--- numbersGame.js
+++ numbersGame.js
@@ -1,7 +1,10 @@
 const Stars = (props) => {
   return (
     <div>
-      ...
+      <i className="fa fa-star"></i>
+      <i className="fa fa-star"></i>
+      <i className="fa fa-star"></i>
+      <i className="fa fa-star"></i>
     </div>
   );
 }
