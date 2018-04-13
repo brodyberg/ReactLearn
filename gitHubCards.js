b671c55ed40d4647fbbfb40f51c57afa0e8cce01
Brody Berg <brodyberg@gmail.com>
2018-04-13 09:46:37 -0700

    Re-usable Card

--- gitHubCards.js
+++ gitHubCards.js
@@ -1,10 +1,10 @@
 const Card = (props) => {
 	return (
   	<div style={{margin: '1em'}}>
-    	<img width="75" src="https://avatars0.githubusercontent.com/u/151955?v=4" />
+    	<img width="75" src={props.avatar_url} />
       <div style={{display: 'inline-block', marginLeft: 10}}>
-      	<div style={{fontSize: '1.25em', fontWeight: 'bold'}}>Brody Berg</div>
-        <div>Microsoft</div>
+      	<div style={{fontSize: '1.25em', fontWeight: 'bold'}}>{props.name}</div>
+        <div>{props.company}</div>
       </div>
     </div>
   );
@@ -13,7 +13,10 @@ const Card = (props) => {
 const CardList = (props) => {
 	return (
   	<div>
-    	<Card />
+    	<Card 
+          name="Brody Berg" 
+          avatar_url="https://avatars0.githubusercontent.com/u/151955?v=4"
+          company="Microsoft" />
     	<Card />
     	<Card />
     </div>
