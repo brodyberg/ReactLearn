134e289b4b8cc0e441e7fbab40211d32bb432df9
Brody Berg <brodyberg@gmail.com>
2018-04-13 09:51:14 -0700

    Build cards from list using map and Spread operator

--- gitHubCards.js
+++ gitHubCards.js
@@ -10,6 +10,21 @@ const Card = (props) => {
   );
 };
 
+let data = [
+  { name: "Brody Berg", 
+    avatar_url: "https://avatars0.githubusercontent.com/u/151955?v=4",
+    company: "Microsoft"
+  },
+  { name: "Chuck Finley", 
+    avatar_url: "https://avatars2.githubusercontent.com/u/561447?v=4",
+    company: "Stanford Edu"
+  },
+  { name: "Emelyn Berg", 
+    avatar_url: "https://avatars3.githubusercontent.com/u/22876892?v=4",
+    company: ""
+  },
+]
+
 const CardList = (props) => {
 	return (
   	<div>
