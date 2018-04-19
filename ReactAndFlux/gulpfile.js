433328792bf346a151e62d2a88b1d22c4defe784
Brody Berg <brodyberg@gmail.com>
2018-04-18 17:31:14 -0700

    Remove eslint since it does not work anyway

--- ReactAndFlux/gulpfile.js
+++ ReactAndFlux/gulpfile.js
@@ -76,23 +76,10 @@ gulp.task('Create CSS build artifacts', function() {
     .pipe(gulp.dest(config.paths.dist + '/css'));
 })
 
-gulp.task('Lint Javascript and JSX', function () {
-  return 
-    gulp
-      .src(['src/main.js','!node_modules/**'])
-//      .src(['**/*.js','!node_modules/**'])
-//      .src(config.paths.mainJs)
-      //.src(config.paths.js)
-      .pipe(eslint())
-//      .pipe(eslint({ config: '.eslintrc.json' }))
-      .pipe(eslint.format())
-      .pipe(eslint.failAfterError());
-});
-
 gulp.task('Watch HTML and Javascript for changes', function() {
   gulp.watch(config.paths.html, ['Create HTML build artifacts']);
   gulp.watch(config.paths.css, ['Create CSS build artifacts']);
-  gulp.watch(config.paths.js, ['Create and bundle Javascript artifacts', 'Lint Javascript and JSX']);
+  gulp.watch(config.paths.js, ['Create and bundle Javascript artifacts']);
 });
 
 // what this is doing is creating the default tasks
@@ -103,7 +90,6 @@ gulp.task(
   'default', 
   [ 'Create HTML build artifacts'
   , 'Create CSS build artifacts'
-  , 'Lint Javascript and JSX'
   , 'Create and bundle Javascript artifacts'
   , 'Open the browser'
   , 'Watch HTML and Javascript for changes']);
