997602f4de4736aceef79b502f10c3c5832a80b7
Brody Berg <brodyberg@gmail.com>
2018-04-19 16:41:43 -0700

    Wrap part of main in an "iffy" so we can use strict

--- ReactAndFlux/src/main.js
+++ ReactAndFlux/src/main.js
@@ -8,36 +8,38 @@ var ReactDOM = require('react-dom');
 var Home = require('./components/homePage');
 var About = require('./components/about/aboutPage');
 
+(function(win) {
+  "use strict"
+
+  class App extends React.Component {
+    render () {
+      var Child; 
+  
+      switch (this.props.route) {
+        case 'about': Child = About; break;
+        default: Child = Home;
+      }
+  
+      return (
+        <div>
+          <Child />
+        </div>      
+      );
+    }
+  }
+  
+  // Does not recommend this method for production
+  function renderx() {
+    var route = window.location.hash.substr(1);
+  
+    ReactDOM.render(<App route={route} />, document.getElementById('app'));
+  }
+  
+  window.addEventListener('hashchange', renderx);
+  renderx();
+  
+  // this is the entry point for the app
+})(window);
+
 // goal is to reference different components based
 // on the url
-
-class App extends React.Component {
-  render () {
-    var Child; 
-
-    switch (this.props.route) {
-      case 'about': Child = About; break;
-      default: Child = Home;
-    }
-
-    return (
-      <div>
-        <Child />
-      </div>      
-    );
-  }
-}
-
-// Does not recommend this method for production
-function renderx() {
-  var route = window.location.hash.substr(1);
-
-  ReactDOM.render(<App route={route} />, document.getElementById('app'));
-}
-
-window.addEventListener('hashchange', renderx);
-renderx();
-
-// this is the entry point for the app
-
-
