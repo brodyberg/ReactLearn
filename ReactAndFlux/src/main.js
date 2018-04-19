fa91a5b6dc222fd4ca173526aefe1d8a605e83b3
Brody Berg <brodyberg@gmail.com>
2018-04-19 16:33:42 -0700

    About page which we show by url

--- ReactAndFlux/src/main.js
+++ ReactAndFlux/src/main.js
@@ -6,8 +6,36 @@ $ = jQuery = require('jquery');
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
 
-ReactDOM.render(<Home />, document.getElementById('app')); 
+// goal is to reference different components based
+// on the url
+
+class App extends React.Component {
+  render () {
+    var Child; 
+
+    switch (this.props.route) {
+      case 'about': Child = About; break;
+      default: Child = Home;
+    }
+
+    return (
+      <div>
+        <Child />
+      </div>      
+    );
+  }
+}
+
+function renderx() {
+  var route = window.location.hash.substr(1);
+
+  ReactDOM.render(<App route={route} />, document.getElementById('app'));
+}
+
+window.addEventListener('hashchange', renderx);
+renderx();
 
 // this is the entry point for the app
 
