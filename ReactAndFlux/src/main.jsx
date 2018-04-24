a50a6599828a38034fe11276d39e2a71ebcc9dc9
Brody Berg <brodyberg@gmail.com>
2018-04-23 21:09:51 -0700

    Cleanup, but still no navigation

--- ReactAndFlux/src/main.jsx
+++ ReactAndFlux/src/main.jsx
@@ -6,50 +6,6 @@ $ = jQuery = require('jquery');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-// var Route = require('react-router-dom').Route;
-
 var App = require('./app.jsx');
-// var HomePage = require('./components/homePage.jsx');
-// var AuthorPage = require('./components/authors/authorPage.jsx');
-// var AboutPage = require('./components/about/aboutPage.jsx');
-
-// const PrimaryLayout = () => (
-//   <div className="primaryLayout">
-//     <header>
-//       Our React Router 4 App
-//     </header>
-//     <main>
-//       <Route
-//         path="/"
-//         exact
-//         component={Home} />
-//       <Route
-//         path="/about"
-//         component={AboutPage} />
-//       <Route
-//         path="/authors"
-//         component={AuthorPage} />
-//     </main>
-//   </div>
-// )
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
-
-
-
-
-// var React = require('react');
-
-
-
-// var ReactRouter = require('react-router');
-// var Router = ReactRouter.Router;
-// var Route = ReactRouter.Route;
-
-// var createBrowserHistory  from 'history/lib/createBrowserHistory'
-// let history = createBrowserHistory()
-
-// var ReactDOM = require('react-dom');
-
-//ReactDOM.render(<Router>{routes}</Router>, document.getElementById('app'));
