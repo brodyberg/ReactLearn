5bba0febbc04c4ef0e3892514cd16761d7ee5278
Brody Berg <brodyberg@gmail.com>
2018-04-23 21:27:42 -0700

    Some links work
    Links produced in the BrowserRouter work but the ones manually created do not and I think it has to do with server vs client-side rendering

--- ReactAndFlux/src/app.jsx
+++ ReactAndFlux/src/app.jsx
@@ -1,33 +1,48 @@
 "use strict"
 
 var BrowserRouter = require('react-router-dom').BrowserRouter;
+//var Router = require('react-router-dom').Router;
+var Route         = require('react-router-dom').Route;
+var Link          = require('react-router-dom').Link;
+
 var Header        = require('./components/common/header.jsx');
 var React         = require('react');
-var Route         = require('react-router-dom').Route;
 var HomePage      = require('./components/homePage.jsx');
 var AuthorPage    = require('./components/authors/authorPage.jsx');
 var AboutPage     = require('./components/about/aboutPage.jsx');
 
 const PrimaryLayout = () => {
   return (
-    <div className="primaryLayout">
-      <Header />
-      <header>
+    // <Router>
+      <div className="primaryLayout">
+        <Header />
         Our React Router 4 App
-      </header>
-      <main>
-        <Route
-          path="/"
-          exact
-          component={HomePage} />
-        <Route
-          path="/about"
-          component={AboutPage} />
-        <Route
-          path="/authors"
-          component={AuthorPage} />
-      </main>
-    </div>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/authors">Authors</Link>
+            </li>
+          </ul>
+          <hr/>
+          <Route
+            path="/"
+            exact
+            component={HomePage} />
+          <Route
+            path="/about"
+            component={AboutPage} />
+          <Route
+            path="/authors"
+            component={AuthorPage} />
+        </div>
+      </div>
+    // </Router>
   );
 }
 
