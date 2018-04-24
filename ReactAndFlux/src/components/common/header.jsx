46fb698b17126fcc9fa7328287f857e87ce4ffa0
Brody Berg <brodyberg@gmail.com>
2018-04-24 16:21:01 -0700

    Convert last anchor to Link

--- ReactAndFlux/src/components/common/header.jsx
+++ ReactAndFlux/src/components/common/header.jsx
@@ -1,18 +1,18 @@
 "use strict"
 
 var React = require('react');
-var Link          = require('react-router-dom').Link;
-var nav = require('bootstrap').nav;
+var Link  = require('react-router-dom').Link;
+var nav   = require('bootstrap').nav;
 
 class Header extends React.Component {
   render() {
     return ( 
       <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="#">
+          <Link className="navbar-brand" to="/">
             <img src="images/pluralsight_logo.webp" style={{marginRight: 5}} width="30" height="30" alt="" />
             Pluralsight
-          </a>
+          </Link>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
