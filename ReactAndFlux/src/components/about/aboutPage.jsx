e5d003326bf4676779dcd2acd921ffe6de607327
Brody Berg <brodyberg@gmail.com>
2018-04-25 16:05:50 -0700

    Notes on keyboard driving experiment

--- ReactAndFlux/src/components/about/aboutPage.jsx
+++ ReactAndFlux/src/components/about/aboutPage.jsx
@@ -3,6 +3,17 @@
 var React = require('react');
 
 class About extends React.Component {
+
+// we can start with a state
+// of number highlighted
+// and provide the arrows such that
+// pressing an arrow will change the
+// one highlighted (with looping)
+// and then, right/left can (loop)
+// change colors. All of the above 
+// change the state of the component and
+// trigger re-renders
+
   render() {
     return (
       <div>
