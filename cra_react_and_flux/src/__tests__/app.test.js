aa6b1a41608bc58f5846dcdc2bbb1625cf15404c
Brody Berg <brodyberg@gmail.com>
2018-04-26 16:49:37 -0700

    Unify passing tests
    I love the test watcher

--- cra_react_and_flux/src/__tests__/app.test.js
+++ cra_react_and_flux/src/__tests__/app.test.js
@@ -1,6 +1,14 @@
-import App from '../App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
 
-it('adds two to a number', () => {
+it('Renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('Adds two to a number', () => {
   expect(App.PlusTwo(0)).toEqual(2);
   expect(App.PlusTwo(3)).toEqual(5); 
 });
