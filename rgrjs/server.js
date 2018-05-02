fa2d32287631e8087ca3feef19eb90841f38f5f5
Brody Berg <brodyberg@gmail.com>
2018-05-01 17:08:13 -0700

    Pull data from Mongo via route
    http://localhost:3000/data/links will show:
    
    [{"_id":"5ae7a30507bf8637b03082a0","id":"1","title":"React.js Main website","url":"https://facebook.github.io/react"},{"_id":"5ae7a39b7dfa95101430adb3","id":"2","title":"Relay.js main website","url":"https://facebook.github.io/relay"}]

--- rgrjs/server.js
+++ rgrjs/server.js
@@ -4,19 +4,11 @@ import express from 'express'
 import morgan from 'morgan'
 import {MongoClient} from 'mongodb'
 
-let app = express(); 
-
-// app.use(function (req, res, next) {
-//   console.log('Time:', Date.now());
-//   console.log('Req: ' + req.method);
-//   console.log('Req: ' + req.params);
-//   next();
-// })
-
-app.use(morgan('combined'))
-
 let port = 3000; 
 
+let app = express(); 
+
+app.use(morgan('combined'))
 app.use(express.static('public'));
 
 //app.get('/', (req, res) => res.send('hello brody!'));
@@ -38,16 +30,16 @@ MongoClient
     { 
       console.log("Server running on https://localhost:" + port)
     });
-  
-    // rgrjs_db
-    //   .collection("links")
-    //   // this means "find everything" 
-    //   .find({})
-    //   .toArray((err, links) => {
-    //     if (err) throw err;
-  
-    //     console.log(links);
-    //   });
-
-    //   mongoClient.close();
   });
+
+app.get("/data/links", (req, res) => {
+  rgrjs_db
+    .collection("links")
+    // this means "find everything" 
+    .find({})
+    .toArray((err, links) => {
+      if (err) throw err;
+      
+      res.json(links);
+    });
+});
