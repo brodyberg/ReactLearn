d5687c4cdb09f974b4943a80ad0bdbb08f8d6500
Brody Berg <brodyberg@gmail.com>
2018-05-01 17:00:04 -0700

    Log express requests
    Use morgan express.js middleware to log requests to our server in Apache format.

--- rgrjs/server.js
+++ rgrjs/server.js
@@ -1,41 +1,53 @@
 "use strict";
 
-import express from 'express';
-import {MongoClient} from 'mongodb';
+import express from 'express'
+import morgan from 'morgan'
+import {MongoClient} from 'mongodb'
 
 let app = express(); 
+
+// app.use(function (req, res, next) {
+//   console.log('Time:', Date.now());
+//   console.log('Req: ' + req.method);
+//   console.log('Req: ' + req.params);
+//   next();
+// })
+
+app.use(morgan('combined'))
+
+let port = 3000; 
 
 app.use(express.static('public'));
 
 //app.get('/', (req, res) => res.send('hello brody!'));
 
-app.listen(3000);
-
-let userPass = process.env.MONGO_PASS;
+let rgrjs_db; 
 
 MongoClient
   .connect("mongodb://brodybrgrjs.documents.azure.com:10255/?ssl=true", {
     auth: {
       user: 'brodybrgrjs',
-      password: userPass,
+      password: process.env.MONGO_PASS,
     }
   }, 
   function(err, mongoClient) { 
-
     if (err) throw err;
 
-    // by default we'll connect to the wrong database
-    var rgrjs_db = mongoClient.db("rgrjs");
+    rgrjs_db = mongoClient.db("rgrjs");
+    app.listen(port, () => 
+    { 
+      console.log("Server running on https://localhost:" + port)
+    });
   
-    rgrjs_db
-      .collection("links")
-      // this means "find everything" 
-      .find({})
-      .toArray((err, links) => {
-        if (err) throw err;
+    // rgrjs_db
+    //   .collection("links")
+    //   // this means "find everything" 
+    //   .find({})
+    //   .toArray((err, links) => {
+    //     if (err) throw err;
   
-        console.log(links);
-      });
+    //     console.log(links);
+    //   });
 
-      mongoClient.close();
+    //   mongoClient.close();
   });
